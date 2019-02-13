691d38497ef3e8ac018148ab4ff82206de4b809f
Rafal <rafaldoranczyk@gmail.com>
2019-02-13 20:43:58 +0100

    Updates

---
+++ precache-manifest.172fb01626d6e9e2a90d419f933067b0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "955032a6fe80a48ae2a7",
+    "url": "/hangmanGame/static/js/runtime~main.955032a6.js"
+  },
+  {
+    "revision": "e553afef56d415b8d460",
+    "url": "/hangmanGame/static/js/main.e553afef.chunk.js"
+  },
+  {
+    "revision": "05178af70466777f6a8b",
+    "url": "/hangmanGame/static/js/2.05178af7.chunk.js"
+  },
+  {
+    "revision": "c6518ecf3dbbf23de908ac2439f08b98",
+    "url": "/hangmanGame/index.html"
+  }
+];
