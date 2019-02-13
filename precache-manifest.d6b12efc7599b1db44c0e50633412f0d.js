e6a84280ee02dac6f6a86e88f55640e4aedbb8ba
Rafal <rafaldoranczyk@gmail.com>
2019-02-13 21:02:13 +0100

    Updates

---
+++ precache-manifest.d6b12efc7599b1db44c0e50633412f0d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffea54f9820407bf2837",
+    "url": "/hangman-game/static/js/runtime~main.ffea54f9.js"
+  },
+  {
+    "revision": "b354297a6a39e6effafa",
+    "url": "/hangman-game/static/js/main.b354297a.chunk.js"
+  },
+  {
+    "revision": "8c0daedf57cf366fb4b7",
+    "url": "/hangman-game/static/js/2.8c0daedf.chunk.js"
+  },
+  {
+    "revision": "66e3fcef9182c1c8eb0424ff0dba01b6",
+    "url": "/hangman-game/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/hangmanGame/precache-manifest.c9db44a264c36ddbeef11c9929ce7f44.js"
+  "/hangman-game/precache-manifest.d6b12efc7599b1db44c0e50633412f0d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/hangmanGame/index.html", {
+workbox.routing.registerNavigationRoute("/hangman-game/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
